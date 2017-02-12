75a8c416dfcf8260efe72e1816f5e480d2839094
Martin Helmich <kontakt@martin-helmich.de>
2017-02-12 18:10:00 +0100

    Add locationCreated event

---
+++ src/contracts/events/location_created.go
@@ -0,0 +1,14 @@
+package events
+
+// LocationCreatedEvent is emitted whenever a location is created
+type LocationCreatedEvent struct {
+	ID      string `json:"id"`
+	Name    string `json:"name"`
+	Address string `json:"address"`
+	Country string `json:"country"`
+}
+
+// EventName returns the event's name
+func (c *LocationCreatedEvent) EventName() string {
+	return "locationCreated";
+}
