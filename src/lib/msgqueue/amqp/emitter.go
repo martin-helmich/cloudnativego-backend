72b178a578969ede1400477d59f48e59f3dd2672
Martin Helmich <kontakt@martin-helmich.de>
2017-03-12 13:57:32 +0100

    Make amqpEventEmitter simpler

--- src/lib/msgqueue/amqp/emitter.go
+++ src/lib/msgqueue/amqp/emitter.go
@@ -11,9 +11,9 @@ import (
 )
 
 type amqpEventEmitter struct {
-	channel  *amqp.Channel
-	exchange string
-	events   chan *emittedEvent
+	connection *amqp.Connection
+	exchange   string
+	events     chan *emittedEvent
 }
 
 type emittedEvent struct {
@@ -50,68 +50,53 @@ func NewAMQPEventEmitterFromEnvironment() (msgqueue.EventEmitter, error) {
 // accepting the connection as a parameter and then creating our own private
 // channel is the safest way to ensure this).
 func NewAMQPEventEmitter(conn *amqp.Connection, exchange string) (msgqueue.EventEmitter, error) {
-	channel, err := conn.Channel()
-	if err != nil {
-		return nil, fmt.Errorf("could not create AMQP channel: %s", err)
-	}
-
 	emitter := amqpEventEmitter{
-		channel:  channel,
-		exchange: exchange,
-		events:   make(chan *emittedEvent),
+		connection: conn,
+		exchange:   exchange,
 	}
 
-	// Normally, all(many) of these options should be configurable.
-	// For our example, it'll probably do.
-	err = channel.ExchangeDeclare(exchange, "topic", true, false, false, false, nil)
+	err := emitter.setup()
 	if err != nil {
 		return nil, err
 	}
 
-	// AMQP channels are not thread-safe. Since this struct's `Emit` function
-	// could be called from anywhere, we have `Emit` simply write into a channel
-	// and have a single go-routine read from that channel and do the actual
-	// publishing.
-	go func() {
-		for item := range emitter.events {
-			emitter.emitItem(item)
-		}
-	}()
-
 	return &emitter, nil
 }
 
-func (e *amqpEventEmitter) emitItem(item *emittedEvent) {
-	defer close(item.errorChan)
-
-	// TODO: Alternatives to JSON? Msgpack or Protobuf, maybe?
-	jsonBody, err := json.Marshal(item.event)
+func (a *amqpEventEmitter) setup() error {
+	channel, err := a.connection.Channel()
 	if err != nil {
-		item.errorChan <- fmt.Errorf("could not JSON-serialize event: %s", err)
-		return
+		return err
 	}
 
-	pub := amqp.Publishing{
-		Headers: amqp.Table{
-			"x-event-name": item.event.EventName(),
-		},
+	defer channel.Close()
+
+	// Normally, all(many) of these options should be configurable.
+	// For our example, it'll probably do.
+	err = channel.ExchangeDeclare(a.exchange, "topic", true, false, false, false, nil)
+	return err
+}
+
+func (a *amqpEventEmitter) Emit(event msgqueue.Event) error {
+	channel, err := a.connection.Channel()
+	if err != nil {
+		return err
+	}
+
+	defer channel.Close()
+
+	// TODO: Alternatives to JSON? Msgpack or Protobuf, maybe?
+	jsonBody, err := json.Marshal(event)
+	if err != nil {
+		return fmt.Errorf("could not JSON-serialize event: %s", err)
+	}
+
+	msg := amqp.Publishing{
+		Headers:     amqp.Table{"x-event-name": event.EventName()},
 		ContentType: "application/json",
 		Body:        jsonBody,
 	}
 
-	err = e.channel.Publish(e.exchange, item.event.EventName(), false, false, pub)
-	if err != nil {
-		item.errorChan <- fmt.Errorf("could not publish message to AMQP: %s", err)
-	}
-}
-
-func (e *amqpEventEmitter) Emit(event msgqueue.Event) error {
-	errChan := make(chan error)
-	item := &emittedEvent{
-		event:     event,
-		errorChan: errChan,
-	}
-
-	e.events <- item
-	return <-errChan
+	err = channel.Publish(a.exchange, event.EventName(), false, false, msg)
+	return err
 }
