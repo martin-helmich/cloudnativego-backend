bb74062d5785c1731350069260b22a6d927c536d
Martin Helmich <kontakt@martin-helmich.de>
2017-03-19 11:35:10 +0100

    Make routing more RESTful

--- src/eventservice/rest/rest.go
+++ src/eventservice/rest/rest.go
@@ -14,7 +14,7 @@ func ServeAPI(endpoint string, dbHandler persistence.DatabaseHandler, eventEmitt
 	r := mux.NewRouter()
 	eventsrouter := r.PathPrefix("/events").Subrouter()
 	eventsrouter.Methods("GET").Path("/{SearchCriteria}/{search}").HandlerFunc(handler.findEventHandler)
-	eventsrouter.Methods("GET").Path("/all").HandlerFunc(handler.allEventHandler)
-	eventsrouter.Methods("POST").Path("/New/").HandlerFunc(handler.newEventHandler)
+	eventsrouter.Methods("GET").Path("").HandlerFunc(handler.allEventHandler)
+	eventsrouter.Methods("POST").Path("").HandlerFunc(handler.newEventHandler)
 	return http.ListenAndServe(endpoint, r)
 }
